c383b8701ec75a09916280200702a9473756b981
Karee <thisiskaree@gmail.com>
2026-05-03 01:50:31 +0530

    Question_6_a_6_d.cpp

--- Lab - 9/Question_6_a_6_d.cpp
+++ Lab - 9/Question_6_a_6_d.cpp
@@ -7,29 +7,22 @@ int main() {
         fout << ch;
     }
     fout.close();
-
     fstream file("alpha.txt", ios::in | ios::out);
     if (!file) {
         cout << "Error opening file!";
         return 1;
     }
-
     char ch;
-
     file.seekg(9);
     file.get(ch);
     cout << "10th character: " << ch << endl;
-
     file.seekp(4);   
     file.put('X');
-
     file.seekg(0, ios::end);
     cout << "File size: " << file.tellg() << " bytes" << endl;
-
     file.seekg(-1, ios::end);
     file.get(ch);
     cout << "Last character: " << ch << endl;
-
     file.close();
     return 0;
 }
