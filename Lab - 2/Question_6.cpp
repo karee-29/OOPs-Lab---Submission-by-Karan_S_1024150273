3fc998b623aa484e1c8928d71f03f6352716b245
Karee <thisiskaree@gmail.com>
2026-05-03 01:24:37 +0530

    Question_6.cpp

--- Lab - 2/Question_6.cpp
+++ Lab - 2/Question_6.cpp
@@ -1,34 +1,24 @@
 #include <iostream>
 using namespace std;
-
-// Global variable
 int x = 100;
-
 class Demo
 {
 private:
-    static int count; // static variable
-
+    static int count; 
 public:
-    void displayMessage(); // function declared here (defined outside)
-    
+    void displayMessage(); 
     void showLocalGlobal(int x)
     {
         cout << "Local x = " << x << endl;
-        cout << "Global x = " << ::x << endl; // access global x using scope resolution
+        cout << "Global x = " << ::x << endl;
     }
-
     void showStatic()
     {
-        count++; // access static variable
+        count++; 
         cout << "Static count = " << count << endl;
     }
 };
-
-// Define static variable outside the class using scope resolution
 int Demo::count = 0;
-
-// Define function outside the class using scope resolution
 void Demo::displayMessage()
 {
     cout << "Function defined outside the class using scope resolution!" << endl;
@@ -37,21 +27,12 @@ void Demo::displayMessage()
 int main()
 {
     Demo d;
-
-    // (a) Call class function defined outside the class
     d.displayMessage();
-
-    // (b) Access global variable with same name as local variable
-    int x = 50; // local variable
+    int x = 50;
     d.showLocalGlobal(x);
-
-    // (c) Access static variable
     d.showStatic();
     d.showStatic();
-
-    // (d) Use inbuilt libraries with scope resolution operator
     std::cout << "Using std::cout with scope resolution" << std::endl;
     std::cin.ignore(); // using std::cin as example
-
     return 0;
 }
