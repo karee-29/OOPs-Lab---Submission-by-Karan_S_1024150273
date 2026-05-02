9713decf603ee82b850811032123e9458eac9e35
Karee <thisiskaree@gmail.com>
2026-05-03 01:29:48 +0530

    Question_2.cpp

--- Lab - 4/Question_2.cpp
+++ Lab - 4/Question_2.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
 using namespace std;
-
 class Rectangle {
 private:
     int length;
     int breadth;
-
 public:
     Rectangle(int l = 0, int b = 0) {
         length = l;
@@ -13,7 +11,6 @@ public:
         cout << "Constructor called (Length = " << length 
              << ", Breadth = " << breadth << ")" << endl;
     }
-
     ~Rectangle() {
         cout << "Destructor called" << endl;
     }
@@ -22,20 +19,16 @@ public:
         return length * breadth;
     }
 };
-
 int main() {
     Rectangle r[3] = {
         Rectangle(),
         Rectangle(5),
         Rectangle(4, 6)
     };
-
     cout << "\nAreas of Rectangles:\n";
-
     for(int i = 0; i < 3; i++) {
         cout << "Rectangle " << i + 1 
              << " Area = " << r[i].area() << endl;
     }
-
     return 0;
 }
