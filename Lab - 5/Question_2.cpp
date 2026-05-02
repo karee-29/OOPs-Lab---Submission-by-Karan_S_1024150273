683c980d2b92f502b1eca14e1780f94f005b9b5b
Karee <thisiskaree@gmail.com>
2026-05-03 01:31:24 +0530

    Question_2.cpp

--- Lab - 5/Question_2.cpp
+++ Lab - 5/Question_2.cpp
@@ -1,25 +1,20 @@
 #include <iostream>
 using namespace std;
-
 class Base {
 protected:
     int number;
-
 public:
     Base(int n) {
         number = n;
     }
 };
-
 class Derived : public Base {
 public:
     Derived(int n) : Base(n) {}
-
     void show() {
         cout << "Protected number accessed in Derived class: " << number << endl;
     }
 };
-
 int main() {
     Derived obj(50);
     obj.show();
