7d98e38dbaf44926bf426410710e135c81a35c03
Karee <thisiskaree@gmail.com>
2026-05-03 01:50:44 +0530

    Question_6_e.cpp

--- Lab - 9/Question_6_e.cpp
+++ Lab - 9/Question_6_e.cpp
@@ -7,23 +7,18 @@ int main() {
     fout << "This is a test file\n";
     fout << "C++ File Handling\n";
     fout.close();
-
     ifstream fin("data.txt");
     if (!fin) {
         cout << "Error opening file!";
         return 1;
     }
-
     fin.seekg(9);  
-
     cout << "Current position: " << fin.tellg() << endl;
-
     char ch;
     cout << "\nRemaining content:\n";
     while (fin.get(ch)) {
         cout << ch;
     }
-
     fin.close();
     return 0;
 }
